7b78fc1341a060f3f74f44374cf8c15e57f0b4e0
Thato Mongwe <thatomongwe47@gmail.com>
2024-04-07 13:50:24 +0200

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,8 @@ def safe_print_list(my_list=[], x=0):
             if count < x:
                 print(item, end=' ')
                 count += 1
-        print()
-        return count
-    except:
-        pass
-
+                print()
+                return count
+                except:
+                    pass
+                    
