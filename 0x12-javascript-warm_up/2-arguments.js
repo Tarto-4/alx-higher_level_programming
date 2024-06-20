35b9c1a664ae32ec18c1d379fcdfbdfd5b8ebac2
Thato Mongwe <thatomongwe47@gmail.com>
2024-06-20 12:18:20 +0000

    Message depending on the number of arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,14 +2,10 @@
 
 const argCount = process.argv.length - 2;
 
-if (argCount === 0)
-{
+if (argCount === 0) {
   console.log('No argument');
-} else if (argCount === 1)
-  {
-    console.log('Argument found')
-  } else
-  {
-    console.log('Arguments found')
-  }
-  
+} else if (argCount === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
