4a6fd94df11a5a9e9e1068ea8518d0b246a463ee
Thato Mongwe <thatomongwe47@gmail.com>
2024-07-06 15:32:26 +0200

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Sends a POST request to the passed URL with the email as a parameter."""
-
-import urllib.request
-import urllib.parse
+"""Displays the X-Request-Id header variable of a request to a given URL.
+Usage: ./1-hbtn_header.py <URL>
+"""
 import sys
+import urllib.request
 
-data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
 
-with urllib.request.urlopen(sys.argv[1], data) as response:
-    print(response.read().decode('utf-8'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
