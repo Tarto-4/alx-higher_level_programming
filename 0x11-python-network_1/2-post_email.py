8606c1e0c4dce83f4f1d4e6b512b41b5b8146ccc
Thato Mongwe <thatomongwe47@gmail.com>
2024-07-06 15:29:40 +0200

    Sends a POST request to the passed URL with the email as a parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-Usage: ./2-post_email.py <URL> <email>
-  - Displays the body of the response.
-"""
-import sys
-import urllib.parse
+"""Sends a POST request to the passed URL with the email as a parameter."""
+
 import urllib.request
+import urllib.parse
+import sys
 
+data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
-
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+with urllib.request.urlopen(sys.argv[1], data) as response:
+    print(response.read().decode('utf-8'))
