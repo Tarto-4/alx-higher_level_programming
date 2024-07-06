40038faadf249b8229f14da96f8e1f0aa078c4d9
Thato Mongwe <thatomongwe47@gmail.com>
2024-07-06 13:34:14 +0200

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display the body of the response from the URL passed as argument if the request was successful
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q "200" && curl -s "$1"
+# send a GET request to an URL with curl, and display the body of the response
+curl -sL "$1"
